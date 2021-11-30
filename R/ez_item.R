b69cdf4c4c1ffa8fcf3303e8cde3768d5343afcd
cysics <cysics@gmail.com>
2021-11-30 09:48:47 +0900

    init version

--- R/ez_item.R
+++ R/ez_item.R
@@ -1,5 +1,19 @@
+#' Item Analysis
+#'
+#' @param data Data of item
+#' @param method Method like "2PL", "graded", and "auto"
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' data <- survey[5:10]
+#' strsplit(data)
+
 ez_item <- function(data, method="auto"){
-    models <- data %>% mutate_all(~as.factor(.)) %>% map(levels) %>% map(length) %>% unlist() %>% data.frame() %>%
-        rename(levels=1) %>% mutate(levels=ifelse(levels=="2", "2PL", "graded")) %>% pull(levels)
+    models <- data %>% mutate_all(~as.factor(.)) %>% map(levels) %>%
+        map(length) %>% unlist() %>% data.frame() %>%
+        rename(levels=1) %>% mutate(levels=ifelse(levels=="2", "2PL", "graded")) %>%
+        pull(levels)
     models
 }
